e01ae9b62520307d5674c933ea3d665314b50277
dennts <dennisvanderlinden@gmail.com>
2019-10-20 13:14:01 +0200

    second commit

---
+++ mainuifunction.py
@@ -0,0 +1,12 @@
+from PySide2 import QtCore, QtGui, QtWidgets
+from mainui import Ui_MainWindow
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
