53969011961763c824961dd854bab10b4be8e4fb
dennts <dennisvanderlinden@gmail.com>
2019-10-20 14:49:34 +0200

    second commit

--- mainuifunction.py
+++ mainuifunction.py
@@ -1,6 +1,27 @@
 from PySide2 import QtCore, QtGui, QtWidgets
 from mainui import Ui_MainWindow
 
+class UIPracticeWindow(QtWidgets.MainWindow):
+	def __init__(self, parent=None):
+		super(UIPracticeWindow, self).__init__(parent)
+
+		self.ui = Ui_MainWindow()
+		self.ui.setupUi(self)
+
+		# User functions
+		self.ui.pushButton.clicked.connect(self.printDennts)
+		self.ui.clear.clicked.connect(self.cleartext)
+		self.ui.pushButton.clicked.connect(self.printMessage)
+
+	def printDennts(self):
+		self.ui.textEdit.setText(" Dennts")
+	
+	def cleartext(self):
+		self.ui.textEdit.setText("")
+
+	def printMessage(self):
+		print ("Hello World")
+
 
 if __name__ == "__main__":
     import sys
