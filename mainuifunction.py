bc22a507740223eb24e398a2d5a359af09c1f83a
Dennis van der Linden <dennisvanderlinden@gmail.com>
2019-10-21 18:12:55 +0200

    Thanks Jeroen

--- mainuifunction.py
+++ mainuifunction.py
@@ -2,25 +2,25 @@ from PySide2 import QtCore, QtGui, QtWidgets
 from mainui import Ui_MainWindow
 
 class UIPracticeWindow(QtWidgets.QMainWindow):
-	def __init__(self, parent=None):
-		super(UIPracticeWindow, self).__init__(parent)
+    def __init__(self, parent=None):
+        super(UIPracticeWindow, self).__init__(parent)
 
-		self.ui = Ui_MainWindow()
-		self.ui.setupUi(self)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
 
-		# User functions
-		self.ui.pushButton.clicked.connect(self.printDennts)
-		self.ui.clear.clicked.connect(self.cleartext)
-		self.ui.pushButton.clicked.connect(self.printMessage)
+        # User functions
+        self.ui.pushButton.clicked.connect(self.printDennts)
+        self.ui.clear.clicked.connect(self.cleartext)
+        self.ui.pushButton.clicked.connect(self.printMessage)
 
-	def printDennts(self):
-		self.ui.textEdit.setText(" Dennts")
-	
-	def cleartext(self):
-		self.ui.textEdit.setText("")
+    def printDennts(self):
+        self.ui.textEdit.setText(" Dennts")
+    
+    def cleartext(self):
+        self.ui.textEdit.setText("")
 
-	def printMessage(self):
-		print("Hello World")
+    def printMessage(self):
+        print("Hello World")
 
 
 if __name__ == "__main__":
@@ -28,4 +28,5 @@ if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = UIPracticeWindow()
     MainWindow.show()
-    #sys.exit(app.exec_())
+    sys.exit(app.exec_())
+    
